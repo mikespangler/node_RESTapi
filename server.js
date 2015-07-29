621a5a34b6069c0c07695292e3dc84a4fc827a89
Mike Spangler <spangler.mike@gmail.com>
2015-07-29 10:17:57 -0700

    all api routes set

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var port       = process.env.PORT || 8080
 // Connect to DB
 mongoose.connect('mongodb://localhost:27017/api');
 
+// Pull in User Model
+var User = require('./app/models/user');
 
 // Body Parser allows use to grab info from POST requests 
 
@@ -34,9 +36,75 @@ app.get('/', function(req,res){
 
 var apiRouter = express.Router();
 
-apiRouter.get('/',function(req,res){
-  res.json({ message: 'welcome to the api!'})
+apiRouter.use(function(req,res,next){
+  console.log('We have a piping hot request!')
+  next();
 });
+
+apiRouter.get('/',function(req,res){
+  res.json({ message: 'welcome to the api!'});
+});
+
+apiRouter.route('/users')
+
+  .post(function(req, res){
+    var user = new User();
+
+    user.name     = req.body.name;
+    user.username = req.body.username;
+    user.password = req.body.password;
+
+    user.save(function(err){
+      if (err) {
+        if (err.code == 11000) { // duplicate entry
+          return res.json({ success: false, message: 'A user with that\ username already exists. '});
+        } else {
+          return res.send(err);
+        }
+      }
+
+      res.json({ message: 'User Created!' });
+    })
+  })
+
+  .get(function(req,res){
+    User.find(function(err, users){
+      if (err) res.send(err);
+      res.json(users);
+    });
+  });
+
+apiRouter.route('/users/:user_id')
+  .get(function(req,res){
+    User.findById(req.params.user_id, function(err,user){
+      if (err) res.send(err);
+      res.json(user);
+    })
+  })
+
+  .put(function(req,res){
+    User.findById(req.params.user_id, function(err,user){
+      if (err) res.send(err);
+
+      if (req.body.name)     user.name = req.body.name;
+      if (req.body.username) user.username = req.body.username;
+      if (req.body.password) user.password = req.body.password;
+
+      user.save(function(err){
+        if (err) res.send(err);
+        res.json({ message: 'User Updated!' });
+      })
+    })
+  })
+
+  .delete(function(req,res){
+    User.remove({
+      _id: req.params.user_id
+    }, function(err, user) {
+      if (err) return res.send(err);
+      res.json({ message: 'Successfully Deleted!'});
+    });
+  })
 
 // Register Routes for API prefix
 app.use('/api', apiRouter);
