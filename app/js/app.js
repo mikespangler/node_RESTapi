8a38153ac46d88919adc6f012520c41c7cabfccf
Mike Spangler <spangler.mike@gmail.com>
2015-08-03 16:07:50 -0700

    ready 2 route angular style

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,7 @@ angular.module('firstApp', [])
   
   .controller('mainController', function(){
     // bind this to view-model
+
     var vm = this;
     vm.message = 'Hey there! Come and see how good I look!';
 
@@ -10,4 +11,20 @@ angular.module('firstApp', [])
       { name: 'Yoga 2 Pro', color: 'Gray', nerdness: 6 },
       { name: 'Chromebook', color: 'Black', nerdness: 5 },
     ];
+
+    //  info that comes from the form
+    vm.computerData = {};
+
+    // add a computer to the list
+    vm.addComputer = function(){
+      vm.computers.push({
+        name: vm.computerData.name,
+        color: vm.computerData.color,
+        nerdness: vm.computerData.nerdness
+      });
+
+      // clear the form
+      vm.computerData = {};
+    };
   });
+  
